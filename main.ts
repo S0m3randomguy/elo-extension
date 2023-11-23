f130b241218abd6b2183dfa544ae88bf4012275a
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-11-23 22:03:59 +0100

    Added Elo calculation block

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 
+enum GameScore {
+    Win = 1,
+    Draw = 0.5,
+    Lose = 0
+}
+
+//% color=190 weight=0 icon="\uf1ec" block="Elo"
 namespace elo {
-    enum GameScore {
-        Win = 1,
-        Draw = 0.5,
-        Lose = 0
-    }
-    
-    //% block="game of rating $a vs $b result $score|| with coefficient $k"
-    function calculate(a: number, b: number, score: GameScore, k: number=20): number {
+    //% block="change from rating $a vs $b $score|| with coefficient $k"
+    //% a.defl=100 b.defl=100
+    //% k.defl=20
+    //% k.min=10
+    //% k.max=40
+    //% inlineInputMode=inline
+    //% blockId=elo_calculate
+    export function calculate(a: number, b: number, score: GameScore, k: number=20): number {
         return Elo.calculate(a, b, score, k);
     }
 }
